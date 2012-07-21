6509038714d58aff75112cfeed61d90cb9385a7e
Stephen Oney <swloney@gmail.com>
2012-07-21 16:38:05 -0400

    jsep -> esprima

--- src/red_obj/red_cell.js
+++ src/red_obj/red_cell.js
@@ -2,12 +2,27 @@
 var jsep = red.jsep, cjs = red.cjs, _ = cjs._;
 
 var RedCell = function(str, parent) {
-	this.str = str;
-	this.tree = jsep(this.str, {
-		keywords: ["true", "false", "this", "STAY"]
-	});
+	this.set_str(str);
 };
 (function(my) {
+	var proto = my.prototype;
+	proto.set_str = function(str) {
+		this._str = str;
+		this._update_tree();
+		this._update_value();
+		return this;
+	};
+	proto.get_str = function() { return this._str; };
+
+	proto._update_tree = function() {
+		this._tree = jsep(this.get_str(), {
+			keywords: ["true", "false", "this", "STAY"]
+		});
+	};
+
+	proto._update_value = function() {
+		console.log(this._tree);
+	};
 }(RedCell));
 
 red.create_cell = function(str, parent) {
