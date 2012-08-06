270c6e76760a026c7e7b38feba5f2ddfda289127
Stephen Oney <swloney@gmail.com>
2012-08-05 23:58:43 -0400

    Shadows

--- src/core/shadow_statechart.js
+++ src/core/shadow_statechart.js
@@ -1,25 +1,55 @@
 (function(red) {
 var cjs = red.cjs, _ = cjs._;
 
-red._shadow_statechart = function(statechart) {
-	var shadow = statechart.clone();
+var do_shadow = function(statechart, shadow) {
+	var substate_names = statechart.get_substate_names();
+	_.forEach(substate_names, function(substate_name) {
+		var substate = statechart.get_state_with_name(substate_name);
+		var shadow_substate;
+		if(shadow.has_state(substate_name)) {
+			shadow_substate = shadow.get_state_with_name(substate_name);
+		} else {
+			shadow_substate = substate.clone();
+			shadow.add_state(substate_name, shadow_substate);
+		}
 
-	shadow.equivalent_state = function(state) {
-	};
-
+		do_shadow(substate, shadow_substate);
+	});
 	statechart	._on("state_added", function(event) {
-					console.log("state added", event);
+					var substate_name = event.state_name
+						, substate = event.state;
+
+					if(shadow.has_state(substate_name)) {
+						shadow_substate = shadow.get_state_with_name(substate_name);
+					} else {
+						shadow_substate = substate.clone();
+						shadow.add_state(substate_name, shadow_substate);
+					}
+
+					do_shadow(substate, shadow_substate);
 				})
 				._on("state_removed", function(event) {
-					console.log("state removed", event);
+					var substate_name = event.state_name
+						, substate = event.state;
+
+					if(shadow.has_state(substate_name)) {
+						shadow.remove_state(substate_name);
+					}
 				})
 				._on("transition_added", function(event) {
 					console.log("transition added", event);
 				})
 				._on("transition_removed", function(event) {
 					console.log("transition removed", event);
+				})
+				._on("destroy", function(event) {
+					console.log("destroyed");
 				});
+};
 
+red._shadow_statechart = function(statechart) {
+	var shadow = statechart.clone();
+	do_shadow(statechart, shadow);
 	return shadow;
 };
 }(red));
--- test/unit_tests/skeleton_objs.js
+++ test/unit_tests/skeleton_objs.js
@@ -5,11 +5,24 @@ module("OBJ Skeleton");
 
 test('Shadow Statecharts', function() {
 	expect(0);
+	var ab = cjs.create_event("manual");
+	var bax = cjs.create_event("manual");
 	var sc = cjs.create("statechart")
 				.add_state("A")
-				.add_state("B");
+				.add_state("A.x")
+				.add_state("B")
+				.starts_at("B")
+				.add_transition("A", "B", ab)
+				.add_transition("B", "A.x", bax);
 	var shadow = red._shadow_statechart(sc);
+
+	sc.add_state("B.y");
+	sc.add_state("C.W.Z");
+
+	sc.remove_state("C.W.Z");
+
 	console.log(sc.stringify());
+	console.log(shadow.stringify());
 });
 
 }());
