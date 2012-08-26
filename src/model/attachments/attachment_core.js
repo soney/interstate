ab7f43ab25ff1c16487f7fbea4c31f100611be0c
Stephen Oney <swloney@gmail.com>
2012-08-26 13:51:35 -0400

    Adding attachments

---
+++ src/model/attachments/attachment_core.js
@@ -0,0 +1,18 @@
+(function(red) {
+var cjs = red.cjs, _ = cjs._;
+
+var id = 0;
+var RedAttatchment = function(options) {
+	options = options || {};
+};
+
+(function(my) {
+	var proto = my.prototype;
+}(RedAttachment));
+
+red.RedAttachment = RedAttachment;
+cjs.define("red_attachment", function(options) {
+	var attachment = new RedAttachment(options);
+	return attachment;
+});
+}(red));
