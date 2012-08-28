8dd21b20a1adfaef5e0f0ca4c75809ebab71e902
Stephen Oney <swloney@gmail.com>
2012-08-28 16:58:45 -0400

    Added cells

--- src/view/cell_view.js
+++ src/view/cell_view.js
@@ -1,110 +1,153 @@
 (function(red, $) {
 var cjs = red.cjs, _ = cjs._;
+var state = {
+	IDLE: 0
+	, EDITING: 1
+};
 
 $.widget("red.cell", {
 	
-});
-
-}(red, jQuery));
-/*
-(function(Euclase) {
-var array_utils = Euclase._array_utils;
-$.widget(
-"euc.euc_cell_view",
-{
-	// default options
 	options: {
-		cell: null,
-		debug: false,
-		parent_update_queue: Euclase._update_queue
-	},
-	_create: function() {
-		var self = this,
-			options = self.options,
-			element = self.element,
-			cell = options.cell;
-		
+		cell: undefined
+		, immediate_edits: true
+		, execute_generated_commands: true
+	}
 
-		if(cell.update_queue!=null) {
-			self.option("parent_update_queue", cell.update_queue);
-		}
+	, _create: function() {
+		this._state = state.IDLE;
 
-		self.update_queue = options.parent_update_queue.add_queue({type: "Cell", element: element});
-
-		var king_cell = null;
-		if(Euclase.environment) {
-			king_cell = $("div#king_cell");
-		}
-
-		self.display = $("<div />")	.addClass("cell_display")
-									.appendTo(element)
-									.editablelabel({
-										manualValueChange: true,
-										defaultValue: cell.attr("default_display"),
-										otherClickables: "#king_cell",
-										charLimit: 10
-									});
-		self.display.bind("valuechange", function(event) {
-			var old_value = event.oldValue,
-				new_value = event.newValue;
-			if(Euclase.environment) {
-				Euclase.environment.command("change_cell", {cell: cell, new_value: new_value, old_value: old_value});
-			}
-			else {
-				cell.option("text", new_value);
-			}
-		});
-		self.display.bind("startedediting", function(event) {
-			if(king_cell) {
-				king_cell.euc_environment_king_cell("option", {
-					"editingCell": element
-				});
-			}
-		});
-		self.display.bind("stoppedediting", function(event) {
-			if(king_cell) {
-				king_cell.euc_environment_king_cell("option", {
-					"editingCell": null
-				});
-			}
-		});
-
-		self.update_queue.add(function() {
-			self.display.editablelabel("option", {value: cell.attr("text"), defaultValue: cell.attr("default_display")});
-		}, {name: "Update cell text", object: cell});
-
-		self.update_queue.add(function() {
-			var errors = cell.attr("errors");
-			if(errors == null) errors = [];
-			self._set_errors(errors);
-		}, {name: "Update cell errors", object: cell});
-	},
-	destroy: function() {
-		var self = this,
-			options = self.options;
-
-		options.parent_update_queue.remove(self.update_queue);
-		$.Widget.prototype.destroy.apply(self, arguments); // default destroy
-		self.display.editablelabel("destroy");
-		self.display.remove();
-	},
-	_set_errors: function(errors) {
 		var self = this;
+		this.element.addClass("cell")
+					.on("mousedown.red_cell", function(event) {
+						if(self._state === state.IDLE) {
+							event.preventDefault();
+							event.stopPropagation();
+							self._trigger("begin_editing");
+						}
+					})
+					.text(this.option("cell").get_str());
+		if(this.option("immediate_edits")) {
+			this.element.on("cellbegin_editing.red_cell", function() {
+				self.begin_edit();
+			});
+		}
+		if(this.option("execute_generated_commands")) {
+			this.element.on("cellset_cell_command.red_cell", function(event, data) {
+				var command = data.command;
+				command._do();
+			});
+		}
+		this._add_change_listeners(this.option("cell"));
+	}
 
-		if(errors.length == 0) {
-			self.display.removeClass("error");
+	, _setOption: function(key, value) {
+		var old_value = this.option(key);
+		var new_value = value;
+		if(key === "cell") {
+			this._remove_change_listeners(old_value);
+			this._add_change_listeners(new_value);
+		} else if(key === "immediate_edits") {
+			if(old_value) {
+				this.element.off("cellbegin_editing.red_cell");
+			}
+			if(new_value) {
+				this.element.on("cellbegin_editing.red_cell", function() {
+					self.begin_edit();
+				});
+			}
+		} else if(key === "execute_generated_commands") {
+			if(old_value) {
+				this.element.off("cellset_cell_command.red_cell");
+			}
+			if(new_value) {
+				this.element.on("cellset_cell_command.red_cell", function(event, command) {
+					command._do();
+				});
+			}
 		}
-		else {
-			self.display.addClass("error");
+
+		this._super(key, value);
+	}
+
+	, _get_set_cell_command: function(str) {
+		var command = red.command("change_cell", {
+			cell: this.option("cell")
+			, str: str
+		});
+
+		return command;
+	}
+	
+	, _destroy: function() {
+		this.element.removeClass("cell")
+					.off("click.red_cell")
+					.off("cellset_cell_command.red_cell")
+					.off("cellbegin_editing.red_cell");
+	}
+
+	, begin_edit: function() {
+		if(this._state === state.IDLE) {
+			this._state = state.EDITING;
+
+			this._before_editing_value = this.option("cell").get_str();
+			this.element.html("")
+						.addClass("editing");
+
+			this._edit_input = $("<input type='text' />")	.appendTo(this.element)
+															.attr("value", this._before_editing_value)
+															.select()
+															.focus();
+			var self = this;
+			$(window).on("keydown.red_cell", function(event) {
+				if(event.which === 27) { //Esc
+					self.cancel_changes();
+				} else if(event.which === 13) { //Enter
+					self.commit_changes();
+				}
+			});
 		}
-	},
-	accept: function() {
-		this.display.editablelabel("confirmValue");
-	},
-	cancel: function() {
-		this.display.editablelabel("cancel");
+	}
+
+	, cancel_changes: function() {
+		if(this._state === state.EDITING) {
+			this._state = state.IDLE;
+			$(window).off("keydown.red_cell");
+			this._edit_input.remove();
+			this.element.text(this._before_editing_value)
+						.removeClass("editing");
+		}
+	}
+
+	, commit_changes: function(value) {
+		if(this._state === state.EDITING) {
+			this._state = state.IDLE;
+			$(window).off("keydown.red_cell");
+			value = value || this._edit_input.attr("value");
+			this._edit_input.remove();
+			this.element.removeClass("editing");
+
+			var set_cell_command = this._get_set_cell_command(value);
+			this._trigger("set_cell_command", null, {
+				command: set_cell_command
+			});
+		}
+	}
+
+	, _add_change_listeners: function(cell) {
+		cell = cell || this.option("cell");
+		var self = this;
+		this._live_updater = cjs.liven(function() {
+			var cell_str = cell.get_str();
+			self.element.text(cell_str);
+		});
+	}
+
+	, _remove_change_listeners: function(cell) {
+		cell = cell || this.option("cell");
+		this._live_updater.destroy;
+		delete this._live_updater;
 	}
 
 });
-})(Euclase);
-*/
+
+}(red, jQuery));
