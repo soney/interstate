4ece846dabe1cdb2657e85d2ea334f8bf214ec9d
Stephen Oney <swloney@gmail.com>
2013-05-15 21:46:14 -0700

    bleh

--- src/controller/environment/red_environment.js
+++ src/controller/environment/red_environment.js
@@ -73,8 +73,23 @@
 			root_dict.set("width", red.create("cell", {str: "500"}));
 			root_dict.set("height", red.create("cell", {str: "500"}));
 
-			var ellipse = red.create("dict", {has_protos: false});
+			var ellipse = red.create("dict", {has_protos: false, direct_attachments: [red.create("ellipse_attachment")]});
 			shape.set("ellipse", ellipse);
+			ellipse.set("show", red.create("cell", {str: "true"}));
+			ellipse.set("clip_rect", red.create("cell", {str: "null"}));
+			ellipse.set("cursor", red.create("cell", {str: "'default'"}));
+			ellipse.set("cx", red.create("cell", {str: "30"}));
+			ellipse.set("cy", red.create("cell", {str: "50"}));
+			ellipse.set("fill", red.create("cell", {str: "'none'"}));
+			ellipse.set("fill_opacity", red.create("cell", {str: "1.0"}));
+			ellipse.set("opacity", red.create("cell", {str: "1.0"}));
+			ellipse.set("rx", red.create("cell", {str: "50"}));
+			ellipse.set("ry", red.create("cell", {str: "20"}));
+			ellipse.set("stroke", red.create("cell", {str: "'black'"}));
+			ellipse.set("stroke_dasharray", red.create("cell", {str: "''"}));
+			ellipse.set("stroke_opacity", red.create("cell", {str: "1.0"}));
+			ellipse.set("stroke_width", red.create("cell", {str: "1"}));
+			ellipse.set("transform", red.create("cell", {str: "''"}));
 			
 			var image = red.create("dict", {has_protos: false});
 			shape.set("image", image);
