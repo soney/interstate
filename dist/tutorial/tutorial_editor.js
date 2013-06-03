1154f83f8a0643633d6c0ac45e1a55977f9729e9
Stephen Oney <swloney@gmail.com>
2013-06-03 15:01:55 -0400

    Adding tutorial editor

---
+++ dist/tutorial/tutorial_editor.js
@@ -0,0 +1,17 @@
+/*jslint nomen: true, vars: true, white: true */
+/*jshint scripturl: true */
+/*global red,esprima,able,uid,console,window,jQuery,Raphael */
+
+(function (red, $) {
+	"use strict";
+	var cjs = red.cjs,
+		_ = red._;
+	$.widget("red.prop_cell", {
+		options: {
+		},
+		_create: function() {
+		},
+		_destroy: function() {
+		}
+	});
+}(red, jQuery));
