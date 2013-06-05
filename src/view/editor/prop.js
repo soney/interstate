d1dc59a8a77082f59f117537fac909a13a879460
Stephen Oney <swloney@gmail.com>
2013-06-04 21:16:09 -0400

    Change inherit behavior

--- src/view/editor/prop.js
+++ src/view/editor/prop.js
@@ -408,6 +408,8 @@
 		},
 		inherit: function() {
 			var event = new $.Event("command");
+			event.stopPropagation();
+			event.preventDefault();
 			event.command_type = "inherit";
 			event.name = this.option("name");
 			event.client = this.option("obj");
