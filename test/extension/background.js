56d41c86c797d8efbcb72d933c400c7b45ac3bfe
Stephen Oney <swloney@gmail.com>
2013-07-07 00:27:26 -0400

    Debugging memory leaks

---
+++ test/extension/background.js
@@ -0,0 +1,44 @@
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {   
+    if(request.command) {
+		var command = request.command;
+		if(command === "take_snapshot") {
+			var debuggerId = {tabId: sender.tab.id};
+			chrome.debugger.attach(debuggerId, "1.0", function() {
+				if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError); return; }
+				var listener = function(source, method, params) {
+					if(source.tabId === debuggerId.tabId) {
+						if(method === "HeapProfiler.addProfileHeader") {
+							var uid = params.header.uid;
+							chrome.debugger.sendCommand(debuggerId, "Profiler.getHeapSnapshot", { uid: uid }, function() {
+								chrome.debugger.detach(debuggerId, function() {
+									if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError);  return;}
+									sendResponse({uid: uid});
+									console.log("Took snapshot");
+								});
+							});
+							chrome.debugger.onEvent.removeListener(listener);
+						}
+					}
+				};
+				chrome.debugger.onEvent.addListener(listener);
+				chrome.debugger.sendCommand(debuggerId, "HeapProfiler.takeHeapSnapshot", { reportProgress: false }, function() {
+					if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError); }
+				});
+			});
+		} else if(command === "clear_snapshots") {
+			var debuggerId = {tabId: sender.tab.id};
+
+			chrome.debugger.attach(debuggerId, "1.0", function() {
+				if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError); return; }
+				chrome.debugger.sendCommand(debuggerId, "HeapProfiler.clearProfiles", { reportProgress: false }, function() {
+					if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError); }
+					chrome.debugger.detach(debuggerId, function() {
+						if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError);  return;}
+						sendResponse("done");
+						console.log("Cleared Snapshots");
+					});
+				});
+			});
+		}
+    }
+});
--- test/unit_tests/tests.js
+++ test/unit_tests/tests.js
@@ -1,11 +1,49 @@
-/*
-test( "Cell", function() {
-	var c = new red.Cell({str: "1+1"});
-	var val = c.get_value();
-	console.log(c);
+(function() {
+//var cjs = red.cjs;
+var command_id = 0;
+var callbacks = {};
+var do_command = function(command_name, callback) {
+	var id = command_id++;
+	callbacks[id] = callback;
+	window.postMessage({ id: id, type: "FROM_PAGE", command: command_name }, "*");
+};
+var clear_snapshots = function(callback) {
+	do_command("clear_snapshots", callback);
+};
+var take_snapshot = function(callback) {
+	do_command("take_snapshot", callback);
+};
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window) { return; }
+
+    if (event.data.type && (event.data.type == "FROM_EXTENSION")) {
+		var id = event.data.id;
+		var callback = callbacks[id];
+		delete callbacks[id];
+		callback(event.data.response);
+    }
+}, false);
+
+var cjs = red.cjs;
+asyncTest("Constraint allocation", function() {
+	expect(3);
+	clear_snapshots(function() {
+		take_snapshot(function() {
+			var x = cjs.$(1);
+			var y = cjs.$(function() { return x.get() + 1; });
+			equal(y.get(), 2);
+			equal(x.get(), 1);
+			x.get();
+			x.destroy();
+			x = null;
+			y = null;
+			take_snapshot(function() {
+				ok(true, "Make sure nothing was allocated");
+				start();
+			});
+		});
+	});
 });
-*/
-console.log("Tests");
-
-window.postMessage({ type: "FROM_PAGE", command: "snapshot" }, "*");
-
+}());
