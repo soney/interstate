cc37101a99b4350f3f70c56a23b7624095f6efcc
Stephen Oney <swloney@gmail.com>
2013-07-17 09:26:25 -0400

    bleh

--- test/extension/background.js
+++ test/extension/background.js
@@ -1,7 +1,9 @@
+/*
 var listener = function(source, method, params) {
 	console.log("event", arguments);
 };
 chrome.debugger.onEvent.addListener(listener);
+*/
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {   
     if(request.command) {
@@ -11,23 +13,29 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 			console.log(sender.tab.id);
 			chrome.debugger.attach(debuggerId, "1.0", function() {
 				if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError); return; }
+				var snapshot = "";
 				var listener = function(source, method, params) {
 					if(source.tabId === debuggerId.tabId) {
 						if(method === "HeapProfiler.addProfileHeader") {
 							var uid = params.header.uid;
 							//https://code.google.com/p/chromium/issues/detail?id=260749
 							chrome.debugger.sendCommand(debuggerId,"Debugger.enable", { }, function() {
-								chrome.debugger.sendCommand(debuggerId, "HeapProfiler.getHeapSnapshot", { uid: uid }, function() {
-									chrome.debugger.detach(debuggerId, function() {
-										if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError);  return;}
-										chrome.debugger.sendCommand(debuggerId,"Debugger.disable", { }, function() {
-											sendResponse({uid: uid});
+								chrome.debugger.sendCommand(debuggerId, "Profiler.getHeapSnapshot", { uid: uid }, function() {
+									chrome.debugger.sendCommand(debuggerId,"Debugger.disable", { }, function() {
+										chrome.debugger.onEvent.removeListener(listener);
+										chrome.debugger.detach(debuggerId, function() {
+											if(chrome.runtime.lastError) { console.error(chrome.runtime.lastError);  return;}
+											sendResponse({uid: uid, snapshot: snapshot});
 										});
 										console.log("Took snapshot");
 									});
 								});
 							});
-							chrome.debugger.onEvent.removeListener(listener);
+						} else if(method === "HeapProfiler.addHeapSnapshotChunk") {
+							var chunk = params.chunk;
+							//snapshot += chunk;
+						} else if(method === "HeapPropfiler.finishHeapSnapshot") {
+							//chrome.debugger.onEvent.removeListener(listener);
 						}
 					}
 				};
